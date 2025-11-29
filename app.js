47d42cfcb93e39cc4719ae00c3943cd9663efa76
MOHAMMAD-SAIF1 <saifmohammad2237@gmail.com>
2025-11-29 23:32:32 +0530

    Review Listing Opertions

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ejsMate = require("ejs-mate");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema, reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 
 main()
@@ -43,6 +43,16 @@ const validateListing = (req, res, next)=>{
     }else{
         next();
     }
+};
+
+const validateReview = (req, res, next)=>{
+    let {error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=> el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
 }
 
 //Index Route
@@ -59,7 +69,7 @@ app.get("/listings/new", (req, res) => {
 //Show Route
 app.get("/listings/:id", wrapAsync (async (req, res) =>{
     let {id} = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", {listing});
 }));
 
@@ -93,7 +103,7 @@ app.delete("/listings/:id", wrapAsync (async(req, res)=>{
 
 //Reviews
 // Post Route
-app.post("/listings/:id/reviews", async(req, res)=>{
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res)=>{
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
 
@@ -104,8 +114,17 @@ app.post("/listings/:id/reviews", async(req, res)=>{
 
     
     res.redirect(`/listings/${listing._id}`);
-});
+}));
 
+
+//Delete Review Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async( req, res) =>{
+    let { id, reviewId} =req.params;
+    await Listing.findByIdAndUpdate(id, {$pull :{reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}));
 // app.get("/testListing", (req, res) =>{
 //     let sampleListing = new Listing({
 //         title: "My New Villa",
